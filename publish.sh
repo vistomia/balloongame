9b4e365209c6063582d18ba6bdc0f32da35ebad9
vistomia <victor.farias.0108@gmail.com>
2025-06-28 15:49:54 -0300

    fix: publish.sh export

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
-godot --headless --export "Web"
-godot --headless --export "Linux"
-godot --headless --export "Windows"
+godot --headless --export-release "Web"
+godot --headless --export-release "Linux"
+godot --headless --export-release "Windows Desktop"
 
 butler push ./dist/html vistomia/balloongame:html
 butler push ./dist/linux/balloongame_linux-x64.x86_64 vistomia/balloongame:linux
